67cca528a45261e5bce1df7bcd2f7e02fe020513
gaofeng222 <596642721@qq.com>
2023-03-17 10:23:43 +0800

    通信搭建完成

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,63 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="home-TUIKit-main">
+    <div
+      :class="env?.isH5 ? 'conversation-h5' : 'conversation'"
+      v-show="!env?.isH5 || currentModel === 'conversation'"
+    >
+      <TUISearch class="search" />
+      <TUIConversation @current="handleCurrentConversation" />
+    </div>
+    <div class="chat" v-show="!env?.isH5 || currentModel === 'message'">
+      <TUIChat>
+        <h1>欢迎使用腾讯云即时通信IM</h1>
+      </TUIChat>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent, reactive, toRefs } from 'vue'
+import { TUIEnv } from './TUIKit/TUIPlugin'
 
 export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld
+  setup() {
+    const data = reactive({
+      env: TUIEnv(),
+      currentModel: 'conversation'
+    })
+    const handleCurrentConversation = (value: string) => {
+      data.currentModel = value ? 'message' : 'conversation'
+    }
+    return {
+      ...toRefs(data),
+      handleCurrentConversation
+    }
   }
-});
+})
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.home-TUIKit-main {
+  display: flex;
+  height: 100vh;
+  overflow: hidden;
+}
+.search {
+  padding: 12px;
+}
+.conversation {
+  min-width: 285px;
+  flex: 0 0 24%;
+  border-right: 1px solid #f4f5f9;
+}
+.conversation-h5 {
+  flex: 1;
+  border-right: 1px solid #f4f5f9;
+}
+.chat {
+  flex: 1;
+  height: 100%;
+  position: relative;
 }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: false,
+  parallel: true,
+  publicPath: './' // 基本路径
 })
